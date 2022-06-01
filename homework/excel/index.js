c692436a4edbd9fbdfd458ec7375c81084c6641d
ruzanna07 <xachatryanruzanna5>
2022-06-01 13:14:58 +0400

    read from excel file

---
+++ homework/excel/index.js
@@ -0,0 +1,23 @@
+const xlsx = require("xlsx");
+const express = require("express");
+const file = xlsx.readFile("./homework/excel/students.xls");
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res) => {
+    const sheets = file.SheetNames;
+    const students = [];
+    for (let i = 0; i < sheets.length; i++) {
+        const sheetName = sheets[i];
+        const sheetData = xlsx.utils.sheet_to_json(file.Sheets[sheetName]);
+        sheetData.forEach((x) => {
+            students.push(x)});,
+        }
+res.send(students)});
+
+
+
+app.listen(PORT,() => {
+    (`Server listening on port: ${PORT}`);
+})
+
