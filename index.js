2f774955524a3fca2aa2284f87b88615953daff2
ruzanna07 <xachatryanruzanna5>
2022-06-02 19:13:28 +0400

    From Error

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+<!-- From Master -->
 
+const http = require("http");
+const HOSTNAME = "127.0.0.1";
+const PORT = 3000;
 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.end("hello world");
+});
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}`);
+        });
